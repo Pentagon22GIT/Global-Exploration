38b9b8954ba1d3998cb1448dfbd4a756b0b5c79b
はっせー <153830524+Pentagon22GIT@users.noreply.github.com>
2024-03-12 15:19:38 +0900

    Update next1.js

--- next/next1.js
+++ next/next1.js
@@ -1,13 +1,11 @@
 $('#box1').on('inview', function (event, isInView) {
     if (isInView) {
-        //要素が見えたときに実行する処理
         $("#box1 .count-up").each(function () {
-            $(this).prop('Counter', 0).animate({//カウントアップ
+            $(this).prop('Counter', 0).animate({
                 Counter: $(this).text()
             }, {
-                // スピードやアニメーションの設定
-                duration: 4000,//数字が大きいほど変化のスピードが遅くなる。2000=2秒
-                easing: 'swing',//動きの種類。他にもlinearなど設定可能
+                duration: 4000,
+                easing: 'swing',
                 step: function (now) {
                     $(this).text(Math.ceil(now));
                 }
@@ -18,14 +16,12 @@ $('#box1').on('inview', function (event, isInView) {
 
 $('#box3').on('inview', function (event, isInView) {
     if (isInView) {
-        //要素が見えたときに実行する処理
         $("#box3 .count-down").each(function () {
-            $(this).prop('Counter', 2400000).animate({//カウントアップ
+            $(this).prop('Counter', 2400000).animate({
                 Counter: $(this).text()
             }, {
-                // スピードやアニメーションの設定
-                duration: 4000,//数字が大きいほど変化のスピードが遅くなる。2000=2秒
-                easing: 'swing',//動きの種類。他にもlinearなど設定可能
+                duration: 4000,
+                easing: 'swing',
                 step: function (now) {
                     $(this).text(Math.ceil(now));
                 }
@@ -34,7 +30,6 @@ $('#box3').on('inview', function (event, isInView) {
     }
 });
 
-//値をグラフに表示させる
 Chart.plugins.register({
     afterDatasetsDraw: function (chart, easing) {
         var ctx = chart.ctx;
@@ -43,20 +38,16 @@ Chart.plugins.register({
             var meta = chart.getDatasetMeta(i);
             if (!meta.hidden) {
                 meta.data.forEach(function (element, index) {
-                    // 値の表示
-                    ctx.fillStyle = '#000';//文字の色
-                    var fontSize = 15;//フォントサイズ
-                    var fontStyle = 'normal';//フォントスタイル
-                    var fontFamily = 'Arial';//フォントファミリー
+                    ctx.fillStyle = '#000';
+                    var fontSize = 15;
+                    var fontStyle = 'normal';
+                    var fontFamily = 'Arial';
                     ctx.font = Chart.helpers.fontString(fontSize, fontStyle, fontFamily);
 
                     var dataString = dataset.data[index].toString();
-
-                    // 値の位置
-                    ctx.textAlign = 'center';//テキストを中央寄せ
-                    ctx.textBaseline = 'middle';//テキストベースラインの位置を中央揃え
-
-                    var padding = 5;//余白
+                    ctx.textAlign = 'center';
+                    ctx.textBaseline = 'middle';
+                    var padding = 5;
                     var position = element.tooltipPosition();
                     ctx.fillText(dataString, position.x, position.y - (fontSize / 2) - padding);
 
@@ -68,17 +59,17 @@ Chart.plugins.register({
 
 
 //=========== 円グラフ ============//
-$('#chart01').on('inview', function (event, isInView) {//画面上に入ったらグラフを描画
+$('#chart01').on('inview', function (event, isInView) {
     if (isInView) {
-        var ctx = document.getElementById("chart01");//グラフを描画したい場所のid
+        var ctx = document.getElementById("chart01");
         var chart = new Chart(ctx, {
-            type: 'pie',//グラフのタイプ
-            data: {//グラフのデータ
-                labels: ["飲料系", "冷凍系", "生もの系", "チケット系", "タバコ・酒系", "高価格物品系", "その他"],//データの名前
+            type: 'pie',
+            data: {
+                labels: ["飲料系", "冷凍系", "生もの系", "チケット系", "タバコ・酒系", "高価格物品系", "その他"],
                 datasets: [{
-                    label: "自販機の意識調査",//グラフのタイトル
-                    backgroundColor: ["#ff9900", "#ffff00", "#00ff00", "#00ffff", "#4a86e8", "0000ff", "#b7b7b7"],//グラフの背景色
-                    data: ["90", "4", "2", "1", "1", "1", "1"]//データ
+                    label: "自販機の意識調査",
+                    backgroundColor: ["#ff9900", "#ffff00", "#00ff00", "#00ffff", "#4a86e8", "0000ff", "#b7b7b7"],
+                    data: ["90", "4", "2", "1", "1", "1", "1"]
                 }]
             },
 
